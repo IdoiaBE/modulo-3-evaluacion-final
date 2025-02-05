cf8476274da4b251566a347d2865aecb89fc7df0
IdoiaBE <1992ibe@gmail.com>
2025-02-05 09:27:08 +0100

    header img added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "../styles/Header.scss"
 function Header() {
   return (
     <header className="header">
-        <img src="" alt="" className="header_img"/>
+        <img src="https://akamai.sscdn.co/uploadfile/letras/playlists/9/e/f/0/9ef0c5f0c07b4f32a73e8bcd3d9420e0.jpg" alt="logo de harry potter" className="header_img"/>
     </header>
   )
 }
